236155e40ef0674f0b598bee21d7d615055a5702
Yorshhhh <seinagi77@gmail.com>
2024-07-07 16:09:42 -0400

    commit bienvenida

--- venv2/client/src/components/Bienvenida.jsx
+++ venv2/client/src/components/Bienvenida.jsx
@@ -3,25 +3,9 @@ import React from "react";
 function Bienvenida() {
   return (
     <>
-<<<<<<< HEAD
-      <section
-        className="flex hero bg-cover h-[calc(100vh-30rem)] items-center justify-center"
-        id="inicio"
-      >
-        <h1
-          className="text-white text-6xl font-bold"
-          style={{
-            textShadow:
-              "2px 2px 0 #000, -2px -2px 0 #000, -2px 2px 0 #000, 2px -2px 0 #000",
-          }}
-        >
-          Bienvenidos a LlicaPilsen
-        </h1>
-=======
       <section className="flex hero bg-cover h-[calc(100vh-30rem)] items-center justify-center" id="inicio">
         <h1 className="text-white text-6xl font-bold" style={{ textShadow: '2px 2px 0 #000, -2px -2px 0 #000, -2px 2px 0 #000, 2px -2px 0 #000' }}
         >Bienvenidos a LlicaPilsen</h1>
->>>>>>> 64f876b489f43ec6ea259ef5e77ad717bd0177d7
       </section>
     </>
   );
